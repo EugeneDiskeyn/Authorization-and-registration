b6f2f8a72486dcc11c5f07955d0395a07db14076
Eugene <jdorovko@gmail.com>
2021-11-24 18:07:57 +0300

    Первая часть программы

---
+++ Project/Project/Project.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+	int choice = 0;
+	cout << "1) Login \n2) Registration" << endl;
+	cin >> choice;
+	if (choice == 2) {
+		string username = "", password = "";
+		cout << "Please, enter a username(It can't have spaces):" << endl;
+		cin >> username;
+		cout << endl << "Please, enter a password (It can't have spaces, also try to use signs, letters and numbers to make your password safe enough):" << endl;
+		cin >> password;
+
+		ofstream logsFile;
+		logsFile.open(username + ".txt");
+		logsFile << username << endl << password;
+		logsFile.close();
+
+		cout << "You have successfully been registrated";
+		main();
+	}
+}
