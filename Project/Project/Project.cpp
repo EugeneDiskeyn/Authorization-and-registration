8cd65717de60d802b8489cc0e8aadc4931caaecb
Eugene <jdorovko@gmail.com>
2021-12-03 00:22:39 +0300

    Update

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -15,10 +15,47 @@ int main() {
 
 		ofstream logsFile;
 		logsFile.open(username + ".txt");
-		logsFile << username << endl << password;
+		logsFile << password;
 		logsFile.close();
 
-		cout << "You have successfully been registrated";
+		cout << "You have successfully been registrated" << endl;
 		main();
+	}
+	if (choice == 1) {
+		ifstream Fin;
+		string username1 = "", password1 = "";
+		cout << "Please, enter your username:" << endl;
+		cin >> username1;
+		cout << endl << "Please, enter your password:" << endl;
+		cin >> password1;
+		Fin.open(username1 + ".txt");
+		if (!Fin.is_open()) {
+			cout << endl << "There are no any accounts with such name, come back to the menu?" << endl << "1) Yes" << endl << "2) No" << endl;
+			int yes = 0;
+			cin >> yes;
+			if (yes == 1) {
+				Fin.close();
+				main();
+			}
+			else {
+				return 0;
+			}
+		}
+		else {
+			string str = "";
+			while (!Fin.eof()) {
+				str = "";
+				Fin >> str;
+				if (str != password1) {
+					cout << endl << "The password is wrong. We will send you to the menu" << endl;
+					Fin.close();
+					main();
+				}
+				else {
+					cout << endl << "You have successfully entered"  << endl;
+					return 0;
+				}
+			}
+		}
 	}
 }
